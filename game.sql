b0134f7476e731a683a5d543c79dd40a3a857345
Sid Palas <sid.palas@gmail.com>
2019-09-23 21:02:58 -0700

    Increased and parameterized board size

--- game.sql
+++ game.sql
@@ -3,30 +3,34 @@ WITH recursive rnd_move(move) as (
     *,
     random() rnd
   FROM
-    generate_series(1,9) move
+    generate_series(1,16) move
 ),
 game AS (
   SELECT
     0 AS turn,
     1 AS player_next,
-    3 AS HITS_TO_WIN,
+    6 AS HITS_TO_WIN,
+    16 AS BOARD_SIZE,
     ARRAY[
-      'x','o','o',
-      'x','o','o',
-      'o','o','x',
+      'x','o','o','x',
+      'x','o','o','x',
+      'o','o','x','x',
+      'o','o','o','o',
       --------------
-      'o','x','x',
-      'o','o','o',
-      'o','o','x'
+      'o','x','x','o',
+      'x','o','o','o',
+      'x','o','x','o',
+      'x','o','o','o'
     ] AS true_board,
-    array_fill('-'::text, ARRAY[18]) AS player_board
+    array_fill('-'::text, ARRAY[32]) AS player_board
   UNION(
     SELECT
       turn + 1,
       turn % 2,
       HITS_TO_WIN,
+      BOARD_SIZE,
       true_board,
-      player_board[:(move-1 + player_next * 9)] || true_board[move + player_next * 9] || player_board[(move+1 + player_next * 9):]
+      player_board[:(move-1 + player_next*BOARD_SIZE)] || true_board[move + player_next*BOARD_SIZE] || player_board[(move+1 + player_next*BOARD_SIZE):]
     FROM
       game,
       rnd_move
@@ -37,8 +41,8 @@ game AS (
 game_with_hits AS (
   SELECT
     *,
-    array_length(array_positions(player_board[:9], 'x'), 1) AS hits_0,
-    array_length(array_positions(player_board[10:], 'x'), 1) AS hits_1
+    array_length(array_positions(player_board[:BOARD_SIZE], 'x'), 1) AS hits_0,
+    array_length(array_positions(player_board[(BOARD_SIZE+1):], 'x'), 1) AS hits_1
   FROM game
 ),
 game_with_prev_hits AS (
@@ -54,10 +58,10 @@ results AS (
     player_next,
     hits_0,
     hits_1,
-    true_board[:9] AS tb_0,
-    player_board[:9] AS pb_0,
-    true_board[10:] AS tb_1,
-    player_board[10:] AS pb_1,
+    true_board[:BOARD_SIZE] AS tb_0,
+    player_board[:BOARD_SIZE] AS pb_0,
+    true_board[(BOARD_SIZE+1):] AS tb_1,
+    player_board[(BOARD_SIZE+1):] AS pb_1,
     CASE
       WHEN (hits_0 >= HITS_TO_WIN OR hits_1 >= HITS_TO_WIN)
         THEN 'Player ' || (player_next + 1) % 2 || ' wins!'
@@ -73,9 +77,15 @@ results AS (
 SELECT
   turn,
   (player_next + 1) % 2 AS player,
-  --array_to_string(tb_0[1:3] || chr(10) || tb_0[4:6] || chr(10) || tb_0[7:9] || chr(10), '') true_board_0,
-  array_to_string(pb_0[1:3] || chr(10) || pb_0[4:6] || chr(10) || pb_0[7:9] || chr(10), '') player_board_0,
-  --array_to_string(tb_1[1:3] || chr(10) || tb_1[4:6] || chr(10) || tb_1[7:9] || chr(10), '') true_board_1,
-  array_to_string(pb_1[1:3] || chr(10) || pb_1[4:6] || chr(10) || pb_1[7:9] || chr(10), '') player_board_1,
+  array_to_string(pb_0[1:4] 
+    || chr(10) || pb_0[5:8] 
+    || chr(10) || pb_0[9:12] 
+    || chr(10) || pb_0[13:16] 
+    || chr(10), '') player_board_0,
+  array_to_string(pb_1[1:4] 
+    || chr(10) || pb_1[5:8] 
+    || chr(10) || pb_1[9:12] 
+    || chr(10) || pb_0[13:16] 
+    || chr(10), '') player_board_1,
   winner
 FROM results;
